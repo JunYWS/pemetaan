997320cd7a087bc2af201a59e077ac81b10f4d9b
JunYWS <91828877+JunYWS@users.noreply.github.com>
2021-12-31 16:05:39 +0700

    Add files via upload

---
+++ coba.js
@@ -0,0 +1,18 @@
+const submit_btn = document.querySelector(".submit_btn");
+
+submit_btn.onclick = ()=>{
+    var pem = document.querySelector(.pesan);
+    var nile = document.querySelector(.nilai);
+    if (nile >= 90){
+        document.getElementsByClassName(pesan).innerHTML() = "Kamu lulus";
+    }
+}
+
+    // function submit_btn(){
+    //     var textpem;
+    //     var pem = nilai.value;
+    //     if (pem >= 90){
+    //         textpem = "Kamu Lulus";
+    //         document.getElementById("pesan").innerHTML = textpem;
+    //     }
+    // }
